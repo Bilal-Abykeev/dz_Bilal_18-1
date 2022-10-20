7d17e9868300fb529e02cb1f80c2f4579af68ca7
Bilal <113285404+Bilal-Abykeev@users.noreply.github.com>
2022-10-20 14:32:19 +0300

    Add files via upload

---
+++ DZ3_BILAL_18-1/js/app.js
@@ -0,0 +1,109 @@
+const tabs = document.querySelectorAll(".tabheader__item");
+const tabsParent = document.querySelector(".tabheader__items");
+const tabContent = document.querySelectorAll(".tabcontent");
+
+console.log(tabs);
+console.log(tabsParent);
+console.log(tabContent);
+
+const hideTabContent = () => {
+  tabContent.forEach((item) => {
+    item.style.display = "none";
+  });
+  tabs.forEach((item) => {
+    item.classList.remove("tabheader__item_active");
+  });
+};
+
+const showTabContent = (i  = 0) => {
+  tabContent[i].style.display = "block";
+  tabs[i].classList.add("tabheader__item_active");
+};
+
+hideTabContent();
+showTabContent();
+
+tabsParent.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (target.classList.contains("tabheader__item")) {
+    tabs.forEach((item, i) => {
+      if (target === item) {
+        console.log(i);
+        hideTabContent();
+        showTabContent(i);
+      }
+    });
+  }
+});
+
+let slideIndex = 0;
+
+function autoSlider() {
+  slideIndex++
+  if (slideIndex >= tabs.length) {
+    slideIndex = 0
+  }
+  hideTabContent();
+  showTabContent(slideIndex);
+}
+
+tabsParent.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (target.classList.contains("tabheader__item")) {
+    tabs.forEach((item, i) => {
+      if (target === item) {
+        slideIndex = i;
+        hideTabContent();
+        showTabContent(i);
+      }
+    });
+  }
+});
+
+
+setInterval(autoSlider, 2000);
+
+const modal = document.querySelector(".modal");
+const modalTrigger = document.querySelector(".btn_white");
+const closeModalBtn = document.querySelector(".modal__close");
+
+console.log(modal, "modal");
+console.log(modalTrigger, " modalTrigger");
+console.log(closeModalBtn, " closeModalBtn");
+
+const openModal = () => {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+  document.body.style.overflow = "hidden";
+};
+
+const closeModal = () => {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+  document.body.style.overflow = "";
+};
+
+modalTrigger.addEventListener("click", openModal);
+
+modal.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    closeModal();
+    console.log(event.target);
+  }
+});
+
+function openModalScroll() {
+  const page = document.documentElement;
+
+  if (page.scrollTop + page.clientHeight >= page.scrollHeight) {
+    openModal();
+
+    window.removeEventListener("scroll", openModalScroll);
+  }
+}
+
+window.addEventListener("scroll", openModalScroll)
+
+closeModalBtn.addEventListener("click", closeModal);
